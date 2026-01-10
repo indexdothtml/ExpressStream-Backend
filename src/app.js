f539f32c28482268b8934df11a58bbecd8befc6d
Abhishek Kshirsagar <abhi.kshirsagar1100@gmail.com>
2026-01-10 20:22:11 +0530

    feat: add CORS and cookie-parser middleware, and configure environment variables

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import env from "./env.config.js";
 
 const app = express();
 
@@ -9,5 +13,17 @@ app.use(express.json());
 
 // To parse request send using html form.
 app.use(express.urlencoded({ extended: true }));
+
+// To parse cookies.
+app.use(cookieParser());
+
+// Configuring cors rules.
+
+app.use(
+  cors({
+    origin: env.origin,
+    credentials: true,
+  })
+);
 
 export default app;
---
+++ src/env.config.js
@@ -0,0 +1,6 @@
+const env = {
+  port: process.env.PORT,
+  origin: process.env.ORIGIN,
+};
+
+export default env;
